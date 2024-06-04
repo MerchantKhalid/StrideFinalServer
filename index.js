575bb244f177d65524d36954825b30bf70c71388
Mohammad Khalid Hasan <rabbikhalidhasan@gmail.com>
2024-06-04 08:29:18 +0200

    vercel

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const carsCollection = client
       .db('rabbikhalidhasan')
       .collection('carCollections');
@@ -96,10 +96,10 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 });
-    console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
-    );
+    // await client.db('admin').command({ ping: 1 });
+    // console.log(
+    //   'Pinged your deployment. You successfully connected to MongoDB!'
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
